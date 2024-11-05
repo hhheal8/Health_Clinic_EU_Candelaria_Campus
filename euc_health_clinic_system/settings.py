a50eb20d67e2baa32fb7bc55c8605b3f0fadb19e
hhheal8 <ictgilrovero01000111@gmail.com>
2024-11-06 07:21:03 +0800

    Test pip install MySQL + Apache and setup DATABASES in settings.py

--- euc_health_clinic_system/settings.py
+++ euc_health_clinic_system/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -105,13 +105,23 @@ WSGI_APPLICATION = 'euc_health_clinic_system.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'u579076463_eucclinic2425',
+        'USER': 'u579076463_eucclinic2425',
+        'PASSWORD': 'Lugares@Macatangay19',
+        'HOST': 'srv545.hstgr.io',
+        'PORT': '3306',  # Default port for MySQL
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
